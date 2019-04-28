a59ad45d97cde9752585d1a00ed56801d66b97ec
Daniel E. Markle <dmarkle@ashtech.net>
2019-04-28 05:37:17 -0400

    Initial main class

---
+++ src/main/java/net/ashtech/permissionmap/App.java
@@ -0,0 +1,16 @@
+package net.ashtech.permissionmap;
+
+/**
+ * Permission Map Handling
+ * 
+ * Testing various methodologies for handling permission map handling efficiently
+ *
+ * @author Daniel E. Markle <dmarkle@ashtech.net>
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
